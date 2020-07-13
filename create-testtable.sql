84e59c6fb3d62237c62487747b7bb7c86861e5fd
Ubuntu <ubuntu@ip-172-31-36-96.ec2.internal>
2020-07-13 14:38:10 +0000

    わかりみSQL実践

--- create-testtable.sql
+++ create-testtable.sql
@@ -39,3 +39,105 @@ values(101,'エレン',170,'M')
      ,(104,'ジャン',175,'M')
      ,(105,'サシャ',168,'F')
      ,(106,'コニー',158,'M');
+     
+create table characters(
+    id integer primary key
+,    movie_id integer 
+,    name text
+,    gender char(1)
+);
+
+insert into characters(id, movie_id, name, gender)
+values(401, 93, 'ナウシカ', 'F')
+     ,(402, 94, 'バズー', 'M')
+     ,(403, 94, 'シータ', 'F')
+     ,(404, 94, 'ムスカ', 'M')
+     ,(405, 95, 'さつき', 'F')
+     ,(406, 95, 'メイ', 'F')
+     ,(407, null , 'クラリス', 'F');
+     
+create table students(
+    id integer primary key
+,   name text not null
+,   gender char(1) not null
+,   class text not null
+);
+
+create table test_scores(
+     student_id integer not null references students(id)
+,    subject text not null
+,    score integer not null
+,    primary key (student_id, subject)
+);
+
+select from t.student_id, t.score from test_scores as t where (t.subject, t.score) = (select subject, score from test_scores where subject = '国語' group by subject);
+
+
+select a.subject, a.avg_score from (select subject, avg(score) as avg_score from test_scores group by subject) as a where a.avg_score < 70;
+
+
+select b.subject, b.avg_score
+from (select subject,
+             avg(score) as avg_score
+             from test_scores 
+             group by subject
+) as b where b.avg_score < 70;
+
+with avg_table as(
+    select subject, 
+    avg(score) as avg_score
+    from test_scores
+    group by subject
+)
+
+select * from avg_table where avg_table.avg_score < 70;
+
+with x(max_score) as (
+    select
+    max(score)
+    from test_scores as x
+    where subject = '国語'
+)
+
+select t.student_id, t.score
+from test_scores as t
+where t.subject = '国語' and t.score = (select x.max_score from x);
+
+with max_score(value) as (
+    select max(score)
+    from test_scores
+    where subject = '国語'
+)
+, target(student_id) as (
+    select student_id
+    from test_scores
+    where subject = '国語'
+    and score = (select value from max_score)
+)
+
+select *
+from students
+where id in (select student_id from target);
+
+
+with avg_table(subject, avg_score) as (
+    select subject,
+    avg(score)
+    from test_scores
+    group by subject
+)
+
+select subject, avg_score
+from avg_table
+where avg_score = (select min(a.avg_score) from avg_table a);
+
+create table movies(
+    movie_id integer primary key
+,   title text not null
+);
+
+insert into movies(movie_id, title)
+values(93, '風の谷のナウシカ')
+     ,(94, '天空のしろラピュタ')
+     ,(95, 'となりのトトロ')
+     ,(96, '崖の上のポニョ');
