4f30ddab746a6fa666db32dbc8703621e13deeb8
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-07-20 20:49:58 +0900

    カレンダーをリファクタリング

--- ruby/calender.rb
+++ ruby/calender.rb
@@ -1,11 +1,10 @@
 class Calender
   require 'date'
-  
   def calender(year:,month:,day: 1)
     @@date = Date.new(year,month,day)
     print spaces(space: 8).to_s + @@date.strftime("%B").to_s
     print spaces(space: 2).to_s + @@date.year.to_s + "\n"
-    week_days
+    puts ' Sun Mon Tue Wed Thu Fri Sat'
     set_start_day(year, month, day)
     show_days(year,month,day)
     puts "\n"
@@ -13,33 +12,23 @@ class Calender
   # 呼び出し用メソッド
   
   def spaces(space:)
-      print " " * space
+    print " " * space
   end
   #スペースを挿入するためのメソッド
 
   def set_start_day(year, month, day)
-    array_of_space_without0 = []
-    -4.step(20,4) { |n| array_of_space_without0 << n }
-    array_of_space_without0[0] = 24
-    array_of_space = array_of_space_without0
+    array_of_space = []
+    0.step(24,4) { |n| array_of_space << n }
     array_of_space = array_of_space.map.with_index { |n,i| [i,n] }.to_h
     spaces(space: array_of_space[@@date.wday])
-  end 
-
-  # 月の開始日をスペースで再現する
-
-  def week_days()
-    wd = (0..6).to_a
-    wd.each { |w| printf " #{(Date.new(2020,6,1) + w).strftime("%a")}" }
-    puts "\n"
   end
-  # カレンダーの曜日を表示する
+  # 月の開始日をスペースで再現する
 
   def show_days(year, month, day)
     last_day = Date.new(year, month, -1).day
     day_number = (1..last_day).to_a
     day_number.each do |date|
-      if Date.new(year,month,date).wday != 0
+      if Date.new(year, month, date).wday != 6
         print date.to_s.rjust(4) 
       else
         print date.to_s.rjust(4) + "\n"
