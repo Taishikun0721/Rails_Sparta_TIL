b29ab498c89b4f7c971f0cf784e46516c8722734
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-07-24 18:00:50 +0900

    電話帳アプリ()

--- ruby/name.rb
+++ ruby/name.rb
@@ -10,12 +10,11 @@ name_line.each do |name|
 end
 
 name_array = []
-head_number.each_with_index { |h, i| name_array << [KATAKANA[h], name_line[i]] }
-
-answer = {"ア"=>"イノクチ"}
-test = [[1, 'name2']].to_h
-
-p answer.merge(test) {|key, value3, value4| [value3] + [value4]}.to_a.flatten(1)
-
-name_array.each { |name| p answer.merge([name].to_h) { |key, value, value2| [value] + [value2] }  }
-∑∑
+head_number.each_with_index { |h, i| name_array << [KATAKANA[h], [name_line[i]]] }
+p name_array
+answer = {}
+# test = [[1, 'name2']].to_h
+#
+# p answer.merge(test) {|key, value3, value4| [value3] + [value4]}.to_a.flatten(1)
+#
+# name_array.each { |name| p answer.merge([name].to_h) { |key, value, value2| [value] + [value2] }.to_a.flatten(1) }
