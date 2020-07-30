036f055ed8d5f9b36b5862bb93d26a7159de9b66
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-07-30 22:23:36 +0900

    ログイン機能

--- genbarails/taskleaf/app/models/task.rb
+++ genbarails/taskleaf/app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
   before_validation :set_nameless_name
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
+
+  belongs_to :user
 
   private
 
