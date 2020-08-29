f4fef1cab667ed14c394111772b6dca10c025851
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-08-30 01:10:53 +0900

    Rub力向上トレーニングをリファクタリング&テスト少し追加

--- ruby/summary.rb
+++ ruby/summary.rb
@@ -1,11 +1,35 @@
 
 class Summary
-  def self.show_result
-    input = Array.new(25) { rand 1000 }.each_slice(5).to_a
-    sum_columns = input.transpose.map { |num| num.sum }
-    matrix_with_sum_columns = input.push(sum_columns).flatten(1)
-    matrix_with_sum_columns.each.with_index(1) { |num, i| i % 5 == 0 ? print("#{num} |".rjust(7) + matrix_with_sum_columns[(i - 5)..(i - 1)].sum.to_s.rjust(7) + "\n") : print("#{num} |".rjust(7)) }
+  attr_accessor :base_array, :columns
+
+  def initialize(columns:)
+    if columns <= 15
+      @columns = columns
+    else
+      @columns = 5
+    end
+    @base_array = Array.new(@columns * 5) { rand 1000 }.each_slice(@columns).to_a
   end
+
+  def calculate_matrix_with_summary
+    @matrix_with_rows_sum = add_columns_sum
+    @matrix_with_rows_sum.flatten(1).each.with_index(1) { |num, i| i % @columns == 0 ? print("#{num} |".rjust(7) + @matrix_with_rows_sum[(i - @columns)..(i - 1)].sum.to_s.rjust(7) + "\n") : print("#{num} |".rjust(7)) }
+  end
+
+  private
+
+  def add_columns_sum
+    sum_columns = @base_array.transpose.map { |num| num.sum }
+    (@base_array + sum_columns).flatten(1)
+  end
+
+  # def add_rows_sum
+  #   sum_rows = @base_array.map { |num| num.sum }
+  #   (@input + sum_rows).flatten(1)
+  # end
+  # ※作ってみたけど今回の実装には必要ないので不採用
 end
 
-Summary.show_result
+summary = Summary.new(columns: 15)
+# キーワード引数に何列で表示したいか入力。あまり長くても見辛いので15行以下は強制的に5行で出力。
+summary.calculate_matrix_with_summary
