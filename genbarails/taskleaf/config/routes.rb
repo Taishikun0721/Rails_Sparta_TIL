0932bffe610cc802eec35a2e1652441d3a4cbb31
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-03 21:56:52 +0900

    controller分割テスト前にcommit

--- genbarails/taskleaf/config/routes.rb
+++ genbarails/taskleaf/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources 'tasks' do
+  resources :tasks do
     post :confirm, action: :confirm_new, on: :new
     patch :confirm_edit, action: :confirm_edit, on: :member
+    post :import, on: :collection
   end
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
+
 
   root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
