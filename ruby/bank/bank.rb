14aba4903ad1fad8698c54b5e9df8f71b6937cf8
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-11-24 21:49:57 +0900

    貯金箱プログラムver2.0

--- ruby/bank/bank.rb
+++ ruby/bank/bank.rb
@@ -1,18 +1,22 @@
 class Coin
-  COINS_TYPE = [10, 100, 500]
-
-  def initialize(type)
+  def initialize(type, count)
     @type = type
+    @count = count
   end
 
-  def type
-    @type
+  def calc_money
+    @type * @count
   end
 end
 
-class Bank
-  YES_OR_NO = { y: true, n: false }
+class CoinType
+  TEN = 10
+  ONE_HUNDRED = 100
+  FIVE_HUNDRED = 500
+  ALL_KINDS_OF_COIN = [TEN, ONE_HUNDRED, FIVE_HUNDRED]
+end
 
+class Bank
   attr_accessor :coins
 
   def initialize
@@ -26,7 +30,7 @@ class Bank
       if continue_saving?
         save
       else
-        total
+        puts "貯金したのは#{total}円です"
         exit
       end
     end
@@ -35,14 +39,10 @@ class Bank
   private
 
   def save
-    coin_type = how_much?
-    if Coin::COINS_TYPE.include?(coin_type)
-      coin = Coin.new(coin_type)
-      add(coin)
-    else
-      puts 'コインのタイプが違う'
-      save
-    end
+    coin_type = Ask.how_type_of_money
+    coin_amount = Ask.coin_amount
+    coin = Coin.new(coin_type, coin_amount)
+    add(coin)
   end
 
   def add(coin)
@@ -50,24 +50,42 @@ class Bank
   end
 
   def total
-    puts  "貯金したのは#{@coins.sum(&:type)}円です"
-  end
-
-  def how_much?
-    puts "何円貯金箱に入れますか？ press..10 or 100 or 500"
-    coin_type = gets.to_i
+    @coins.sum(&:calc_money)
   end
 
   def continue_saving?
-    puts '続けて貯金しますか？ press... y or n '
-    answer = gets.chomp.to_sym
-    return continue_saving? unless YES_OR_NO.keys.include?(answer)
-    continue_saving_answer(answer)
+    answer = Ask.continue_saving
+    Ask::YES_OR_NO[answer]
+    # リファクタリングしてて,これAskクラスに映してもいいのかなと思った。
+    # けどstartメソッドの中がこっちの方がすっきりするからラップしていてもいいかも。。
+  end
+end
+
+
+class Ask
+  YES_OR_NO = { y: true, n: false }
+
+  def self.how_type_of_money
+    puts "何円貯金箱に入れますか？ press...#{CoinType::ALL_KINDS_OF_COIN.join(' or ')}"
+    coin_type = gets.to_i
+    return how_type_of_money unless CoinType::ALL_KINDS_OF_COIN.include?(coin_type)
+    coin_type
   end
 
-  def continue_saving_answer(answer)
-    YES_OR_NO[answer]
+  def self.coin_amount
+    puts "何枚入れますか？ press...1 or 2 or number you want"
+    amount = gets.to_i
+    return coin_amount if amount == 0
+    amount
   end
+
+  def self.continue_saving
+    puts "続けて貯金しますか？ press... #{YES_OR_NO.keys.join(' or ')}"
+    answer = gets.chomp.to_sym
+    return continue_saving unless YES_OR_NO.keys.include?(answer)
+    answer
+  end
+  # ユーザーに入力を尋ねる所はAskクラスを作成して切り出した。
 end
 
 bank = Bank.new
